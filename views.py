e677de3b360cd8c171b50b8e39ee265518224103
root <root@battlestar.c.adream420.internal>
2013-09-13 06:14:13 +0000

    deleted:    __init__.py 	modified:   views.py 	battlestar.py

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, redirect, render_template, url_for
 from flask.views import MethodView
-from battlestar.models import Post, Comment
+from models import Post, Comment
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
