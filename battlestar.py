4d7da22da25fbf7a170469f2662b7707cfccf127
root <root@battlestar.c.adream420.internal>
2013-09-13 06:15:09 +0000

    new file:   battlestar.py

---
+++ battlestar.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
+app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {'DB':"battlestar"}
+app.config["SECRET_KEY"] = "kalashinikov"
+
+db = MongoEngine(app)
+
+def register_blueprints(app):
+    from views import posts
+    app.register_blueprint(posts)
+
+register_blueprints(app)
+
+if __name__ == '__main__':
+    app.run()
+
+
