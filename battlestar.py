d1baae67c7d74d0aff67f1542ce79cd1f206e45e
lazygunner <gymgunner@gmail.com>
2013-09-17 17:32:00 +0800

    修改：      battlestar.py 	修改：      templates/admin/detail.html 	修改：      templates/base.html 	修改：      templates/posts/detail.html 	static/

--- battlestar.py
+++ battlestar.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
+from flask.ext.markdown import Markdown
 
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = {'DB':"battlestar"}
 app.config["SECRET_KEY"] = "kalashinikov"
 
 db = MongoEngine(app)
+md = Markdown(app)
 
 def register_blueprints(app):
     from views import posts
