6165d401ea562fdae37eeca5c68fb55bda26025c
lazygunner <gymgunner@gmail.com>
2013-09-23 09:49:57 +0800

    修改：      bridge.py 	修改：      templates/bridge/list.html

--- bridge.py
+++ bridge.py
@@ -59,7 +59,7 @@ class Detail(MethodView):
 
 
 
-class Delete(MethodView):
+class Display(MethodView):
     #decorateors = [requires_auth]
               
 	def get(self, slug):
@@ -70,7 +70,7 @@ class Delete(MethodView):
             "create": slug is None
         }
 		if post:
-			post.show = False
+			post.show = not post.show
 			post.save()
 			return redirect(url_for('bridge.index'))
 		else:
@@ -81,4 +81,4 @@ class Delete(MethodView):
 bridge.add_url_rule('/bridge/', view_func=List.as_view('index'))
 bridge.add_url_rule('/bridge/create/', defaults={'slug': None}, view_func=Detail.as_view('create'))
 bridge.add_url_rule('/bridge/<slug>/', view_func=Detail.as_view('edit'))
-bridge.add_url_rule('/bridge/delete/<slug>/', view_func=Delete.as_view('delete'))
+bridge.add_url_rule('/bridge/display/<slug>/', view_func=Display.as_view('display'))
