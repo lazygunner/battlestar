3363075bd0943dfdbf1fc7c059b2850ba3e89beb
lazygunner <gymgunner@gmail.com>
2013-09-25 11:09:16 +0800

    修改：      views.py

--- views.py
+++ views.py
@@ -2,7 +2,6 @@ from flask.ext.mongoengine.wtf import model_form
 from flask import Blueprint, request, redirect, render_template, url_for
 from flask.views import MethodView
 from models import Post, Comment
-from baidu_pic import getDownloadLink
 import markdown2
 from flask import Markup
 import re
@@ -29,24 +28,10 @@ class DetailView(MethodView):
         }
         return context
 
-    def get_real_link(self, body):
-        p = re.compile('!\[.*?\]\((http://pan.*?)\)')
-        urls = p.findall(body)
-        for url in urls:
-            link = getDownloadLink(url)
-            if link:
-                real_link = link['link']
-                n = re.compile(link)
-                body = n.sub(real_link, body, 1)
-        return body
-
- 
     def get(self, slug):
         context = self.get_context(slug)
         md = markdown2.markdown(context['post'].body)
-        md_body = Markup(md)
-        
-        context['post'].body = self.get_real_link(md_body)
+        context['post'].body = Markup(md)
         return render_template('posts/detail.html', **context)
     
     def post(self, slug):
